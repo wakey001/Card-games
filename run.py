5182f4e0aab640625a1559ff270b16e6b7cfb7c4
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-08 21:37:15 +0000

    added show method but does not seem to work yet

--- run.py
+++ run.py
@@ -7,20 +7,20 @@ class Card():
         print("{} of {}".format(self.value, self.suit))
 
 
-
-
 class Deck():
     def __init__(self):
         self.cards = []
         self.build()
     
     def build(self):
-        for c in ["Hearts", "Clubs", "Diamonds", "Spades"]:
-            for i in range(1, 14):
-                print(i, "of", c)
+        for s in ["Hearts", "Clubs", "Diamonds", "Spades"]:
+            for v in range(1, 14):
+                self.cards.append(Card(s, v))
 
+    def show(self):
+        for c in self.cards:
+            c.show
 
-
-
+        
 deck = Deck()
-
+deck.show()
