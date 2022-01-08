1babcc1a991ea819a84c2c660dfee4f224062f01
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-08 21:02:37 +0000

    added class Card and method show , tested as shown

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Card():
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+   
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
+
+
+card = Card("Diamonds", 8)
+card.show()
