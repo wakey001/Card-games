b7a537ffd762e902d924ceb8cfa8ffe83771423b
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-09 00:17:05 +0000

    clean up some white space problems

--- run.py
+++ run.py
@@ -37,20 +37,17 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
-    
-    
+       
     def draw(self, deck):
         self.hand.append(deck.drawCard())
         return self
     
-
     def showHand(self):
         for card in self.hand:
             card.show()
     
     def discard(self):
         return self.hand.pop()
-
 
 
 deck = Deck()
