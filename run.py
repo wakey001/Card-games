00f812c7d0251f49160f59f61962233a4512cdb2
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-01-15 22:36:30 +0000

    fix wrong comment syntax

--- run.py
+++ run.py
@@ -5,8 +5,9 @@ class Card():
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
-# shows the value and suit of the card if called, eg "8 of hearts" 
+
     def show(self):
+        ''' shows the value and suit of the card if called, eg 8 of hearts'''
         print("{} of {}".format(self.value, self.suit))
 
 
@@ -14,18 +15,22 @@ class Deck():
     def __init__(self):
         self.cards = []
         self.build()
-# Builds the deck by combining the suit(s), and value(v) 
-# This is then put into self.cards empty list then becomes an istance of the class Card  
+  
     def build(self):
+        '''Combines the suit(s), and value(v) and \
+            put into self.cards empty list \
+             then becomes an istance of the class Card'''
         for s in ["Hearts", "Clubs", "Diamonds", "Spades"]:
             for v in range(1, 14):
                 self.cards.append(Card(s, v))
-# A function to show all cards in the deck self.cards list. 
+
     def show(self):
+        '''A function to show all cards in the deck self.cards list.'''
         for c in self.cards:
             c.show()
-#Shuffles the cards in self.cards using the fisher yates method.
+
     def shuffle(self):
+        '''Shuffles the cards in self.cards using the fisher yates method.'''
         for i in range(len(self.cards)-1, 0, -1):
             random_num = random.randint(0, i)
             self.cards[i], self.cards[random_num] = \
@@ -39,16 +44,20 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
-# Draws a card from the deck and puts it in the Players own empty list self.hand.        
+    
     def draw(self, deck):
+        '''Draws a card from the deck and puts\
+         it in the Players own empty list self.hand.'''
         self.hand.append(deck.drawCard())
         return self
-#Shows the cards in the players hand, self.hand.    
+  
     def showHand(self):
+        '''Shows the cards in the players hand, self.hand.'''
         for card in self.hand:
             card.show()
-# Discards a the last card in self.hand.
+
     def discard(self):
+        '''Discards a the last card in self.hand.'''
         return self.hand.pop()
 
 
